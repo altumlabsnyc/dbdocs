fcf385d353efe25f563580b3f7f823a6633e77c3
Alex Godfrey <alex@Alexs-MacBook-Pro-2.local>
2023-07-10 15:49:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# DBDocs Generation Script
+
+This repository contains a script that automatically
+
+### Installation Instructions
+
+1. Request the .env from Alex that contains the connection details to Supabase and DBDocs.
+2. Install the following dependencies after navigating to the project directory:
+   - Make sure you have node installed.
+   - `npm i`
+   - `brew install postgresql@15`
+   - `sudo npm install -g dbdocs`
+   - `sudo npm install -g @dbml/cli`
+
+### Usage
+
+You can now run the script with `npm run start`. A schema.sql and a schema.dbml will be output to the project directory.
---
+++ index.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+const { exec } = require("child_process");
+const fs = require("fs");
+
+// db connection details
+const user = process.env.DB_USER;
+const host = process.env.DB_HOST;
+const database = process.env.DB_DATABASE;
+const password = process.env.DB_PASSWORD;
+const port = process.env.DB_PORT;
+const token = process.env.DBDOCS_TOKEN;
+
+// command to download the schema
+let command = `PGPASSWORD=${password} pg_dump -U ${user} -h ${host} -p ${port} -d ${database} --schema=public -s --no-owner --no-privileges --no-comments`;
+
+// run the pg_dump command
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`Stderr: ${stderr}`);
+    return;
+  }
+
+  // write the output to a file
+  fs.writeFile("schema.sql", stdout, (err) => {
+    if (err) throw err;
+    console.log("Schema has been saved!");
+
+    // Only once the schema.sql file has been written we run the sql2dbml command
+    command = `npx sql2dbml schema.sql --postgres`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`Stderr: ${stderr}`);
+        return;
+      }
+
+      const dbmlFileContent = stdout;
+
+      const projectData = `Project altum {\n\tdatabase_type: 'altum'\n}\n\n`;
+
+      // write the output to a file
+      fs.writeFile("schema.dbml", projectData + dbmlFileContent, (err) => {
+        if (err) throw err;
+        console.log("DBML file has been created!");
+      });
+
+      // Only once the schema.dbml file has been written we run the dbdocs build command
+      command = `DBDOCS_TOKEN=${token} npx dbdocs build schema.dbml`;
+
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`Error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`Stderr: ${stderr}`);
+          return;
+        }
+        console.log(stdout);
+        console.log("DBDocs has been built!");
+      });
+    });
+  });
+});
